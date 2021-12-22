ce3fb9375a0abbb180833bdab72d6454c808363f
Jesus Fernandez <fernandezjesus11@gmail.com>
2021-12-21 22:42:31 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded', function(){
+    const title = document.getElementById('title');
+    const description = document.getElementById('description');
+    const btn = document.getElementById('add');
+    btn.onclick=function(){
+        
+    }
+
+
+})
+
+
+
